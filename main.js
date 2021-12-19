bfebe61bd71290adbca4e507899b384d59fac806
Kurtis Trainor <kurtis.trainor@prodigygame.com>
2021-12-19 11:37:39 -0500

    added electron

---
+++ main.js
@@ -0,0 +1,29 @@
+const { app, BrowserWindow } = require('electron');
+const path = require('path');
+const isDev = require('electron-is-dev');
+
+function createAppWindow() {
+  let win = new BrowserWindow({
+    width: 1280,
+    height: 800,
+    resizable: false,
+    webPreferences: {
+      nodeIntegration: true,
+      enableRemoteModule: true,
+      contextIsolation: false
+    }
+  });
+  win.webContents.on('new-window', (event, url) => {
+    event.preventDefault();
+    win.loadURL(url);
+  });
+  win.loadURL(
+    isDev ? 'http://localhost:3000' : `file://${path.join(__dirname, './build/index.html')}`
+  );
+  win.on('closed', () => {
+    win = null;
+  });
+}
+
+app.on('ready', createAppWindow);
+app.on('window-all-closed', app.quit);
