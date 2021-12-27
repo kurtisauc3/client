42c12bd1e65b8d6310e38f4e33889d47ffa63efd
kurtisauc3 <kurtistrainor@gmail.com>
2021-12-27 18:04:56 -0500

    simplified layout

--- src/pages/Idle/Body.tsx
+++ src/pages/Idle/Body.tsx
@@ -1,21 +1,12 @@
 import React, { FC } from 'react';
 import styled from 'styled-components';
-import Body from './Body';
-import Social from './Social';
 
 const Container = styled.div`
   flex-grow: 1;
-  display: flex;
-  flex-direction: row;
 `;
 
 const Component: FC = () => {
-  return (
-    <Container>
-      <Body />
-      <Social />
-    </Container>
-  );
+  return <Container>Body</Container>;
 };
 
 export default Component;
---
+++ src/pages/Idle/Social.tsx
@@ -0,0 +1,12 @@
+import React, { FC } from 'react';
+import styled from 'styled-components';
+
+const Container = styled.div`
+  width: 200px;
+`;
+
+const Component: FC = () => {
+  return <Container>Social</Container>;
+};
+
+export default Component;
